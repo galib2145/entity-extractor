2257f20c328d5efa5c745c90740f73754511cde7
galib <galib@muktosoft.com>
2017-07-20 17:50:08 +0600

    Added rest endpoints for topic and user frequency

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+const Promise = require('bluebird');
+const path = require('path');
+
+const dataLogic = Promise.promisifyAll(require('./logic/analysis/generic.js'));
+const fileLogic = Promise.promisifyAll(require('./logic/file'));
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+});
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get('/user/:id/:media', function(req, res) {
+  console.log(req.url);
+  const media = req.params.media;
+  const userId = req.params.id;
+
+  dataLogic.getEntityDataForUser(userId, media, (err, resultSet) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+
+    const topicListWithFrequency = resultSet.map((r) => {
+      return {
+        text: r.entity,
+        size: r.count,
+      }
+    });
+
+    res.json(topicListWithFrequency);
+  });
+});
+
+app.get('/users', function(req, res) {
+  console.log(req.url);
+  const userDir = path.join(process.env.HOME, 'entity-analysis-2');
+  const userDirList = fileLogic.getDirectories(userDir);
+  const userList = userDirList.map((dir) => dir.split('/')[4]);
+  res.json(userList);
+});
