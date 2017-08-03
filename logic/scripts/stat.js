40d20e8d875a219f530066ff13224c82ee1be799
galib <galib@muktosoft.com>
2017-08-03 15:19:38 +0600

    Added graph data generation from result

--- logic/scripts/stat.js
+++ logic/scripts/stat.js
@@ -2,20 +2,46 @@ const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const path = require('path');
 
-const resFilePath = path.join(process.env.HOME, '/res/match-sentiment-2');
-fs.readFileAsync(resFilePath)
-  .then((fileContent) => {
-    const resultList = JSON.parse(fileContent);
-    const rrList = resultList.map((r) => {
-      const userId = r.userId;
-      const index = r.res.findIndex((e) => e.user === userId) + 1;
-      return 1 / index;
-    });
+const getGraphDataFromResult = (callback) => {
+  const resFilePath = path.join(process.env.HOME, '/res/match');
+  fs.readFileAsync(resFilePath)
+    .then((fileContent) => {
+      const resultData = JSON.parse(fileContent);
+      const posList = resultData.map((r) => {
+        return r.res.findIndex((e) => e.user === r.userId) + 1;
+      });
 
-    const sumRR = rrList.reduce((prevVal, elem) => prevVal + elem, 0);
-    const mRR = sumRR / rrList.length;
-    console.log(mRR);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+      const graphData = [];
+      for (let i = 1; i <= resultData.length; i++) {
+        const count = posList.filter((p) => p <= i).length;
+        graphData.push([i, count]);
+      }
+
+      callback(null, graphData);
+    })
+    .catch((err) => {
+      callback(err);
+    });;
+};
+
+getGraphDataFromResult((err, res) => {
+  console.log(res);
+})
+
+// const resFilePath = path.join(process.env.HOME, '/res/match-cosine');
+// fs.readFileAsync(resFilePath)
+//   .then((fileContent) => {
+//     const resultList = JSON.parse(fileContent);
+//     const rrList = resultList.map((r) => {
+//       const userId = r.userId;
+//       const index = r.res.findIndex((e) => e.user === userId) + 1;
+//       return 1 / index;
+//     });
+
+//     const sumRR = rrList.reduce((prevVal, elem) => prevVal + elem, 0);
+//     const mRR = sumRR / rrList.length;
+//     console.log(mRR);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
